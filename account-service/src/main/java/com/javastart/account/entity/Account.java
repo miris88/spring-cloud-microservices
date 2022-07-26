fffb6703cf9a960b187519cc8aa5e756e4e92fc5
miris88 <miris88@mail.ru>
2022-07-26 18:02:45 +0300

    account-service

---
+++ account-service/src/main/java/com/javastart/account/entity/Account.java
@@ -0,0 +1,37 @@
+package com.javastart.account.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long accountId;
+    private String name;
+    private String email;
+    private String phone;
+
+    @ElementCollection
+    private List<Long> bills;
+    private OffsetDateTime creationDate;
+
+    public Account(String name, String email, String phone, OffsetDateTime creationDate, List<Long> bills) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.bills = bills;
+        this.creationDate = creationDate;
+    }
+}
